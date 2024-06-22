b44cc614d6fdae8df47cc154d2d85163ef0887c3
ryndrb <49576487+ryndrb@users.noreply.github.com>
2024-06-22 15:06:48 -0400

    Update Chaos Knight's item build

--- FunLib/bot_builds/chaos_knight.lua
+++ FunLib/bot_builds/chaos_knight.lua
@@ -45,20 +45,20 @@ X.BuyList = {
             "item_tango",
             "item_double_branches",
             "item_quelling_blade",
-        
+
             "item_bracer",
             "item_boots",
             "item_magic_wand",
             "item_power_treads",
             "item_armlet",
-            "item_echo_sabre",
-            "item_blink",
-            "item_black_king_bar",--
             "item_orchid",
-            "item_heart",--
+            "item_manta",--
+            "item_black_king_bar",--
             "item_bloodthorn",--
-            "item_harpoon",--
+            "item_heart",--
             "item_aghanims_shard",
+            "item_blink",
+            "item_travel_boots",
             "item_swift_blink",--
             "item_travel_boots_2",--
             "item_moon_shard",
@@ -71,21 +71,19 @@ X.BuyList = {
             "item_tango",
             "item_double_branches",
             "item_quelling_blade",
-        
+
             "item_bracer",
             "item_boots",
             "item_magic_wand",
             "item_power_treads",
             "item_armlet",
-            "item_echo_sabre",
-            "item_blink",
-            "item_black_king_bar",--
             "item_orchid",
+            "item_manta",--
             sUtilityItem,--
-            "item_heart",--
+            "item_black_king_bar",--
             "item_bloodthorn",--
+            "item_heart",--
             "item_aghanims_shard",
-            "item_swift_blink",--
             "item_travel_boots_2",--
             "item_moon_shard",
             "item_ultimate_scepter_2",
@@ -111,7 +109,6 @@ X.SellList = {
             "item_bracer",
             "item_magic_wand",
             "item_armlet",
-            "item_echo_sabre",
         }
     },
     ['pos_4'] = {},
